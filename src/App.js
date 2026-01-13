c1319dc9dcff452a7eb305d3e8a48aecebab7c6a
Jagan Babu Korra <jaganbabukorra@MacBookPro.bbrouter>
2026-01-13 22:35:14 +0530

    CRUD in react TODOlist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Task from './Components/Task';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [todoList, setTodoList] = useState([]);
+const [newTask, setNewTask] = useState("");
+
+const addTask = () => {
+ const task = {
+  id: todoList.length === 0 ? 1 : todoList[todoList.length -1].id + 1,
+  name: newTask,
+  isCompleted: false
+ }
+ setTodoList([...todoList, task]);
+}
+
+const handleTask = (event) => {
+ setNewTask(event.target.value);
+}
+
+const deleteTask = (taskId) => {
+ setTodoList(todoList.filter((task) =>task.id!== taskId ));
+};
+
+const completeTask = (taskId) => {
+ setTodoList(
+  todoList.map((task) => {
+   if (task.id=== taskId) {
+    return {...task, isCompleted: true};
+   }
+   return task;
+  })
+ );
+}
+
+
+return (
+ <div className="App">
+ <div className='addTask'>
+ <input onChange={handleTask}/>
+ <button onClick={addTask}>Add Task</button>
+ </div>
+ <div className='list'>
+  {todoList.map((task) => {
+   return <Task name={task.name} id={task.id} completed = {task.isCompleted} completeTask={completeTask} deleteTask={deleteTask}/>;
+  })}
+ </div>
+ </div>
+
+);
+
 }
 
 export default App;
---
+++ src/Components/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Task = (props) => {
+    return (
+        <div className="task" style={{backgroundColor: props.completed ? "green" : "white"}}>
+            <h1>{props.name}</h1>
+            <button onClick={() => {props.completeTask(props.id)}}>Complete</button>
+            <button onClick={() => {props.deleteTask(props.id)}}>X</button>
+        </div>
+    );
+}
+
+export default Task;
